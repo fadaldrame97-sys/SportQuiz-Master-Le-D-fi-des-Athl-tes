2337651b36b2bd5c4942cf26f407680cd5ae775e
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-03-19 11:00:15 +0000

    Gérer la correction des réponses et changement de couleurs

--- src/composants/Result/ReviewList.jsx
+++ src/composants/Result/ReviewList.jsx
@@ -1,12 +1,23 @@
-export function ListdesReponses({reponses}){
-    return (
-        
-   <div>
-      {reponses.map((q, index) => (
-        <div key={index}>
+export function ListdesReponses({ reponses }) {
+  return (
+    <div>
+      {reponses.map((q, index) => {
+        const corrcte = q.userAnswer === q.correctAnswer;
+
+        let textReponse="Incorrecte";
+        let CouleurReponse="text-red-600"
+        if(corrcte){
+
+            textReponse="Correte"
+            CouleurReponse="text-green-600";
+        }
+
+        return (
+          <div key={index}>
             
-        </div>
-      ))}
+          </div>
+        );
+      })}
     </div>
-    );
+  );
 }
